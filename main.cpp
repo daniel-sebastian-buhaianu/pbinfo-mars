68d4eb56eadb2c6bf0a5fae1701dea9a92f586b5
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-12-28 09:14:26 +0200

    100p

--- main.cpp
+++ main.cpp
@@ -1,34 +1,21 @@
-//#include <fstream>
 #include <iostream>
 using namespace std;
-void mars(int*, int, int, int);
 int main()
 {
-  //ifstream f("main.in");
-  //int n; f >> n;
   int n; cin >> n;
-  int* v = new int[n+1];
-  for (int i = 0; i <= n; i++) v[i] = 0;
-  //int m; f >> m;
+  int* a = new int[n+1];
+  for (int i = 0; i <= n; i++) a[i] = 0;
+  int* b = new int[n+2];
+  for (int i = 0; i <= n+1; i++) b[i] = 0;
   int m; cin >> m;
   for (int i = 0; i < m; i++)
   {
-    int s, d, x;
-    //f >> s >> d >> x;
-    cin >> s >> d >> x;
-    mars(v, s, d, x);
+    int s, d, x; cin >> s >> d >> x;
+    b[s] += x, b[d+1] -= x;
   }
-  //f.close();
-  //ofstream g("main.out");
-  for (int i = 1; i <= n; i++)
-    //g << v[i] << ' ';
-    cout << v[i] << ' ';
-  //g.close();
+  a[1] = b[1];
+  for (int i = 2; i <= n; i++) a[i] = a[i-1] + b[i];
+  for (int i = 1; i <= n; i++) cout << a[i] << ' ';
   return 0;
-}
-void mars(int* v, int s, int d, int x)
-{
-  for (int i = s; i <= d; i++)
-    v[i] += x;
 }
 
